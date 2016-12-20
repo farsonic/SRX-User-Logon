a89219bde6ebf26ad557521cdce745c2393ca69f
Farsonic <fp@prowsehouse.com>
2016-12-20 15:51:07 +1000

    Added OUI lookup for DNSMASQ

--- dnsmask_wrapper.py
+++ dnsmask_wrapper.py
@@ -3,9 +3,13 @@
 import sys
 from sys import argv as arguments
 import subprocess
+from netaddr import *
 
 operation = arguments[1]
-mac = arguments[2]
+mac = EUI(arguments[2])
+oui = mac.oui
+vendor = oui.registration().org
+
 ip = arguments[3]
 
 try:
@@ -17,9 +21,17 @@ posture="Healthy"
 domain="global"
 role="DHCP-Allocated"
 user=hostname
+type="Unknown"
+os="Unknown"
+version="Unknown"
+model="Unknown"
+hostname=user
+
+
+
 
 if operation == "add":
-    subprocess.call([sys.executable, '/var/tmp/User-Logon.py', '-o','logon','-a',ip,'-d',domain,'-r',role,'-u',user,'-p',posture])
+    subprocess.call([sys.executable, '/var/tmp/User-Logon.py', '-o','logon','-a',ip,'-d',domain,'-r',role,'-u',user,'-p',posture,'--vendor',vendor,'-t',type,'--os',os,'--version',version,'--model',model,'--hostname',hostname])
 
 if operation == "del":
     subprocess.call([sys.executable, '/var/tmp/User-Logon.py', '-o','logoff','-a',ip,'-u',user])
